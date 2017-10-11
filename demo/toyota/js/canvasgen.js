f1d641a3df27fc6559850d256d29f93718136b73
jinala <>
2017-10-11 15:49:35 -0400

    Update demo

--- demo/toyota/js/canvasgen.js
+++ demo/toyota/js/canvasgen.js
@@ -11,5 +11,5 @@ function genCanvas(name){
 	}
 	
 	window[name] = Slide(name, readEditor, name+'_navigation', "window." + name);
-	elem.outerHTML = '<canvas id="'+name+'" width="1100" height="1100" style="border:1px solid #000000; " onMouseDown="window.'+name+'.clickDown(event)" onMouseUp="window.'+name+'.clickUp()" onMouseMove="window.'+name+'.clickUp()"></canvas><img src="images/Play.png" height="54" alt="" onClick="window.'+name+'.play()" onLoad="window.'+name+'.grid();"/><div style="font-size:25px"><input id="'+name+'_hasgrid" type="checkbox" onChange="window.'+name+'.gridonoff();">show grid</div><div id="'+name+'_navigation"></div>'
+	elem.outerHTML = '<canvas id="'+name+'" width="1100" height="1800" style="border:1px solid #000000; " onMouseDown="window.'+name+'.clickDown(event)" onMouseUp="window.'+name+'.clickUp()" onMouseMove="window.'+name+'.clickUp()"></canvas><img src="images/Play.png" height="54" alt="" onClick="window.'+name+'.play()" onLoad="window.'+name+'.grid();"/><div style="font-size:25px"><input id="'+name+'_hasgrid" type="checkbox" onChange="window.'+name+'.gridonoff();">show grid</div><div id="'+name+'_navigation"></div>'
 }
